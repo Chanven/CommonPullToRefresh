d41ef96a8531ed76603fa6685a24c399fc992f71
Chanven <chanven0127@gmail.com>
2015-09-23 16:43:50 +0800

    init import

---
+++ CommonPullToRefresh_Demo/gen/com/chanven/commonpulltorefresh/demo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chanven.commonpulltorefresh.demo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CommonPullToRefresh_Lib/gen/com/chanven/commonpulltorefreshview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chanven.commonpulltorefreshview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}

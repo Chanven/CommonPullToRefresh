dab844900029db31e997128695491d9fb5230feb
Chanven <chanven0127@gmail.com>
2015-10-14 09:59:08 +0800

    update README

--- CommonPullToRefresh_Demo/src/com/chanven/commonpulltorefresh/demo/GridViewAtivity.java
+++ CommonPullToRefresh_Demo/src/com/chanven/commonpulltorefresh/demo/GridViewAtivity.java
@@ -1,3 +1,18 @@
+/*
+Copyright 2015 chanven
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package com.chanven.commonpulltorefresh.demo;
 
 import java.util.ArrayList;
@@ -5,6 +20,7 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -20,6 +36,11 @@ import com.chanven.commonpulltorefresh.PtrFrameLayout;
 import com.chanven.commonpulltorefresh.PtrFrameLayout.LoadMoreHandler;
 import com.chanven.commonpulltorefresh.loadmore.GridViewWithHeaderAndFooter;
 
+/**
+ * GridView with loadmore
+ * @author Chanven
+ * @date 2015-10-13 上午9:27:38
+ */
 public class GridViewAtivity extends Activity{
 	PtrClassicFrameLayout ptrClassicFrameLayout;
 	GridViewWithHeaderAndFooter mGridView;
@@ -81,6 +102,7 @@ public class GridViewAtivity extends Activity{
 							mData.add(new String("GridView item -- add" + page));
 						}
 						mAdapter.notifyDataSetChanged();
+						ptrClassicFrameLayout.loadMoreComplete(true);
 						page++;
 						Toast.makeText(GridViewAtivity.this, "load more complete", Toast.LENGTH_SHORT)
 								.show();
@@ -123,6 +145,7 @@ public class GridViewAtivity extends Activity{
 			}
 			TextView textView = (TextView) convertView;
 			textView.setText(datas.get(position));
+			textView.setTextColor(Color.BLACK);
 			return convertView;
 		}
 
--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Android widget with pull to refresh for all the views,and support loadMore for L
 
 在[android-Ultra-Pull-To-Refresh](https://github.com/liaohuqiu/android-Ultra-Pull-To-Refresh)的基础上增加了加载更多的支持，感谢作者
 
-* 下拉刷新支持大部分`view`：`ListView`、`ScrollView`、`WebView`等
-* 加载更多目前只支持`ListView`和`RecyclerView`
+* 下拉刷新支持大部分`view`：`ListView`、`ScrollView`、`WebView`等，甚至一个单独的`TextView`
+* 加载更多目前只支持`ListView`、`RecyclerView`、`GridView`
 * 支持自定义header以及footer
 
 [Demo APK下载](https://github.com/Chanven/CommonPullToRefresh/blob/master/raw/CommonPullToRefresh_Demo.apk?raw=true)
@@ -13,6 +13,8 @@ Android widget with pull to refresh for all the views,and support loadMore for L
  ![Image](https://github.com/Chanven/CommonPullToRefresh/blob/master/raw/listview.gif)
 ####RecyclerView截图
  ![Image](https://github.com/Chanven/CommonPullToRefresh/blob/master/raw/recyclerview.gif)
+####GridView截图
+ ![Image](https://github.com/Chanven/CommonPullToRefresh/blob/master/raw/gridview.gif)
  
 
  具体用法可参见Demo
